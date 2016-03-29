2075e8aad3fc7428ad670bd43e8a5138f590def0
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-29 22:46:39 +0200

    write tests

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
 import test from 'ava';
 import esDepsFromString from './index';
 
-test('should esDepsFromString', (t) =>
-  t.is(esDepsFromString('unicorns'), 'unicorns'));
+const input = `
+// es2015+ modules
+import out from 'out';
+import local from './local';
+
+console.log('modules');
+
+// CommonJS modules
+var qName = require('q');
+var fsName = require('fs');
+var localName = require('./local');
+var n = 1;
+
+require('yo' + 1); // dynamic requires wont work
+
+require('globalImport');
+
+console.log('cjs');`;
+
+const expected = [
+  'out', './local',
+  'q', 'fs', 'globalImport',
+];
+
+test('should esDepsFromString', t =>
+  t.same(esDepsFromString(input), expected));
 
 test('should throw on empty input', t => t.throws(() => { esDepsFromString(); }, TypeError));
 test('should throw on invalid input', t => t.throws(() => { esDepsFromString(2); }, TypeError));
