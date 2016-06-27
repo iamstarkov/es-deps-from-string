a6edb411ab557fb1b954fcd4f4579ce1babf50ed
Vladimir Starkov <iamstarkov@gmail.com>
2016-06-27 14:28:30 +0200

    extract deps to separate funciton

--- index.js
+++ index.js
@@ -14,19 +14,21 @@ const isLiteral = R.pipe(R.prop('arguments'), R.head, R.propEq('type', 'Literal'
 const isLiteralRequire = R.both(isRequire, isLiteral);
 const requireValue = R.pipe(R.prop('arguments'), R.head, R.prop('value'));
 
+const deps = ast => {
+  let list = [];
+  const addToList = item => { list = list.concat(item); };
+  simple(ast, {
+    ImportDeclaration: R.pipe(importValue, addToList),
+    CallExpression: R.when(isLiteralRequire, R.pipe(requireValue, addToList)),
+  });
+  return list;
+};
+
 // esDepsFromString -> String -> Array[String]
 const esDepsFromString = R.unary(R.pipe(
   contract('input', String),
   parse,
-  ast => {
-    let list = [];
-    const addToList = item => { list = list.concat(item); };
-    simple(ast, {
-      ImportDeclaration: R.pipe(importValue, addToList),
-      CallExpression: R.when(isLiteralRequire, R.pipe(requireValue, addToList)),
-    });
-    return list;
-  }
+  deps
 ));
 
 export default esDepsFromString;
